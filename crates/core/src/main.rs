86040cc1cfb9c35c64728f5659be9eb3322f9f95
juha <jpohto@gmail.com>
2026-09-15 21:51:16 +0300

    refactoring + pybinding

--- crates/core/src/main.rs
+++ crates/core/src/main.rs
@@ -5,7 +5,7 @@ use std::fs;
 
 fn main() -> std::io::Result<()> {
 
-    let text = fs::read_to_string("data/rust_guide.md")?;
+    let text = fs::read_to_string("../../data/rust_guide.md")?;
     let tokens = lexer::lex(&text);
     let chunks = chunker::chunk(&tokens);
 
